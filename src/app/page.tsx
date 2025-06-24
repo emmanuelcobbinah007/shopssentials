1d5c0aef94a09160b210d31feaeeb79626a3c5c8
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-06-24 15:29:46 +0000

    implemented poppins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Poppins } from "next/font/google";
 
 import Header from "./components/ui/landing/Header";
 import Hero from "./components/ui/landing/Hero";
 import FeaturedCategories from "./components/ui/landing/FeaturedCategories";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
 const page = () => {
   return (
-    <div className="font-poppins text-[#1A1D23] bg-[#FEFFFE]">
+    <div
+      className={`${poppins.variable} font-poppins text-[#1A1D23] bg-[#FEFFFE]`}
+    >
       <Header />
       <Hero />
       <FeaturedCategories />
